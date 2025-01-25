26ba6c883e521433d8f051936babb992f277cf61
jonathan <jonathhu@stud.ntnu.no>
2025-01-25 14:48:21 +0100

    Initial commit

---
+++ Calculator/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      header: "Header",
+      content: "Content"
+    }
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <h1>{{ header }}</h1>
+    <p>{{ content }}</p>
+  </div>
+</template>
