42c52a6b2d3fda2c4d33425505b8a67f0c951214
jonathan <jonathhu@stud.ntnu.no>
2025-02-02 16:39:22 +0100

    Axios setup

--- Calculator/src/components/Contact.vue
+++ Calculator/src/components/Contact.vue
@@ -1,35 +1,34 @@
 <script setup>
 import { ref, computed } from "vue";
 import { useStore } from "vuex";
+import axios from "axios";
 
 const store = useStore();
 
-const nameField = ref(store.state.name || '');
-const emailField = ref(store.state.email || '');
+const nameField = ref(store.state.name || "");
+const emailField = ref(store.state.email || "");
 const reviewField = ref("");
 
-
 const isValid = computed(() => {
-    return nameField.value && emailField.value && reviewField.value
+  return nameField.value && emailField.value && reviewField.value;
 });
 
 const handleSubmit = () => {
-  
+    const submitResponse = axios.get("http://localhost:3000/form");
 
-  store.commit('setName', nameField.value);
-  store.commit('setEmail', emailField.value);
-
-
+    alert("Response: " + submitResponse);
+  store.commit("setName", nameField.value);
+  store.commit("setEmail", emailField.value);
 };
 </script>
 
 <template>
-    <h2>Contact Form</h2>
-    <form @submit.prevent="handleSubmit">
-      <p>Name: <input type="text" required v-model="nameField" /></p>
-      <p>Email: <input type="text" required v-model="emailField" /></p>
-      <p>Review: <input type="text" required v-model="reviewField" /></p>
-  
-      <button type="submit" :disabled="!isValid">Send form</button>
-    </form>
-  </template>
+  <h2>Contact Form</h2>
+  <form @submit.prevent="handleSubmit">
+    <p>Name: <input type="text" required v-model="nameField" /></p>
+    <p>Email: <input type="text" required v-model="emailField" /></p>
+    <p>Review: <input type="text" required v-model="reviewField" /></p>
+
+    <button type="submit" :disabled="!isValid">Send form</button>
+  </form>
+</template>
