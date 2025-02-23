c151c20076d490e85e973b3596786562f082c185
jonathan <jonathhu@stud.ntnu.no>
2025-02-23 12:29:49 +0100

    Login page template

--- Calculator/src/components/Login.vue
+++ Calculator/src/components/Login.vue
@@ -1,7 +1,31 @@
 <script setup>
 import { ref } from "vue"
+import axiso from "axios";
+
+const username = ref("");
+const password = ref("");
+
+const handleLogin = async () => {
+    if (!username || !password) return;
+
+    try {
+        const response = await axios.post("http://localhost:8080/api/calculator/login", {
+            username: this.username,
+            password: this.password   
+        });
+        const result = response.data;
+    } catch (error) {
+        console.error("Error during login");
+    }
+}
 </script>
 
 <template>
-    <p>This is the login page</p>
+    <h1>Login</h1>
+    <form @submit.prevent="handleLogin">
+        <p>Username: <input type="text" required v-model="username"></p>
+        <p>Password: <input type="text" required v-model="password"></p>
+
+        <button type="submit">Login</button>
+    </form>
 </template>
