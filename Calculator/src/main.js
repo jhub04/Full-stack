377b2b9abc4367fe67de746c3e498fcb1eec297d
jonathan <jonathhu@stud.ntnu.no>
2025-03-07 17:17:11 +0100

    Deleted old store

--- Calculator/src/main.js
+++ Calculator/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from "pinia"
 import router from './router'
-import store from './store';
+
 import piniaPluginPersistedState from "pinia-plugin-persistedstate"
 
 
@@ -12,6 +12,5 @@ pinia.use(piniaPluginPersistedState);
 
 app.use(pinia);
 app.use(router);
-app.use(store);
 
 app.mount('#app');
